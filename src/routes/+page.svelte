857017134a763eb778b1e39dec31d26e8f44fc08
Ivan Carrillo <helloivancar@gmail.com>
2022-10-20 11:05:47 -0700

    Hide add bookmark until clicked

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 
   export let data: PageData
   export let form: ActionData
+  let isCreatingBookmark = false
 
   function deleteBookmark(e) {
     const id = e.detail
@@ -21,14 +22,19 @@
 
 <div>
   Bookmarks
-  <div>
-    <form method="POST" use:enhance>
-      <input name='title' type='text' placeholder="title" class='input' />
-      <input name="url" type="text" placeholder="link" class='input' />
-      <button class='btn bg-blue-500' type="submit">Add Bookmark</button>
-    </form>
-    {#if form?.missing}<div>Please add a url</div>{/if}  
-  </div>
+  <button on:click={() => isCreatingBookmark = true}>Add Bookmark</button>
+  {#if isCreatingBookmark}
+    <div>
+      <form method="POST" use:enhance>
+        <input name='title' type='text' placeholder="title" class='input' />
+        <input name="url" type="text" placeholder="link" class='input' />
+        <button class='btn bg-blue-500' type="submit">Add Bookmark</button>
+        <button on:click={() => isCreatingBookmark = false}>Cancel</button>
+      </form>
+      {#if form?.missing}<div>Please add a url</div>{/if}  
+    </div>
+  {/if}
+
   
   <ul>
     {#each data.bookmarks as link}
