4eabe2081a5f877f50d084ebcf2ffb2b82e4beb4
Ivan Carrillo <helloivancar@gmail.com>
2022-10-25 11:46:21 -0700

    Fix layout on phones

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,8 +1,8 @@
 <script>
-
+  let isOpen = false
 </script>
 
-<div class='bg-zinc-800 h-full text-white'>
+<div class={`bg-zinc-800 h-full text-white ${isOpen ? "block" : "hidden"} absolute sm:static sm:block`}>
   <ul class='flex flex-col justify-center gap-2 mx-1'>
     <li class=''>
       <a href='/' class='flex space-x-2 w-full justify-center items-center '>
@@ -18,5 +18,7 @@
       </a>
     </li>
   </ul>
-  
+</div>
+<div class="absolute bottom-0 left-0 m-4 sm:hidden">
+  <button on:click={() => isOpen = !isOpen} class="btn btn-primary rounded-full py-3 hover:bg-blue-800"><i class="fa-solid fa-bars"></i></button>
 </div>
